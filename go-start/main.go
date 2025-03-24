3b8b5ccfe492ef3a0a4ee32f09175c99a34c11ac
Yellow1945 <Iam.ateist@gmail.com>
2025-03-24 21:05:39 +0300

    24.03.25

--- go-start/main.go
+++ go-start/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+	fmt.Println(
+		ABC(1, 2),
+	)
+
+	_ = gin.Default()
+}
+
+func ABC(a, b int) int {
+	return a + b
 }
